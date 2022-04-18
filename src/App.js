d4490b27885074e01f3fceac635ff22b10228fcf
Anannya Sharma <Anannyasharma8300@gmail.com>
2022-04-18 16:12:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { useState} from "react";
 import { AddStudent } from "./components/AddStudent";
 import { ShowStudents } from "./components/ShowStudents";
+import {useState} from "react"
+import "./App.css"
 
 function App() {
-  const [state, setState] = useState("show")
+  const [state,setState] = useState("display")
 
-  const handleChange =()=>{
-    if(state == "add"){
-      setState("show")
+  const handleChange = () => {
+    if(state==="form"){
+      setState("display")
     }
-    else if(state == "show"){
-      setState("add")
+    else if(state==="display"){
+      setState("form")
     }
   }
+
   return (
     <div className="App">
-      <button className="togglebtn" onClick={handleChange}>{state==="show" ? "Add Data" : "show Data"}</button>
+      <button className="togglebtn" onClick={handleChange}>
+        {state==="display"?"Add a new student ":"Go to students list"}
+      </button>
+      {state==="display"?<ShowStudents />:<AddStudent/>}
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
-      {state ==="show" ? <ShowStudents/>:<AddStudent/>}
       {/* make sure to show either of them do not both together */}
     </div>
   );
--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,133 +1,137 @@
-import { useState, useEffect } from "react";
+import {useState} from "react"
 import "../App.css"
 
+
 export const AddStudent = () => {
+  const [studentData, setStudentData] = useState([]);
 
-    const [form, setForm] = useState([]);
-    const handleChange = (e)=>{
-        const{name, value} = e.target;
-        setForm({...form, [name]:value,})
-    }
+  const handleChange = (e)=>{
+    const{name,value} = e.target;
+    setStudentData({
+      ...studentData,
+      [name]:value
+    })
+  }
 
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        const payload = form;
-        fetch("http://localhost:8080/students", {
-            method:"POST",
-            body: JSON.stringify(payload),
-            headers: {"Content-Type" : "application/json"}
-        }).then(()=>{
-            console.log("Data" , payload)
-        })
-    }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const payload =studentData;
+    fetch("http://localhost:8080/students",{
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers:{"Content-Type": "application/json"}
+    }).then(()=>{
+      console.log(payload)
+    })
+  }
 
-    return (
-      <form onSubmit={handleSubmit} className="addstudent">
+  return (
+    <form className="addstudent" onSubmit={handleSubmit} >
+      <div >
+        Firstname:{" "}
+        <input
+          onChange={handleChange}
+          type="text"
+          name="first_name"
+          className="first_name"
+          placeholder="enter first name"
+        />
+      </div>
+      <div>
+        {" "}
+        Last Name:
+        <input
+          onChange={handleChange}
+          type="text"
+          name="last_name"
+          className="last_name"
+          placeholder="enter last name"
+        />
+      </div>
+      <div>
+        {" "}
+        Email:
+        <input
+          onChange={handleChange}
+          type="email"
+          name="email"
+          className="email"
+          placeholder="enter email"
+        />
+      </div>
+
+      <div>
+        Gender: {/* NOTE: radio boxes only work when they have same `name`. */}
         <div>
-          Firstname:{" "}
+          Male
           <input
-          onChange = {handleChange}
-            type="text"
-            name="first_name"
-            className="first_name"
-            placeholder="enter first name"
-          />
-        </div>
-        <div>
-          {" "}
-          Last Name:
-          <input
-          onChange = {handleChange}
-            type="text"
-            name="last_name"
-            className="last_name"
-            placeholder="enter last name"
-          />
-        </div>
-        <div>
-          {" "}
-          Email:
-          <input
-          onChange = {handleChange}
-            type="email"
-            name="email"
-            className="email"
-            placeholder="enter email"
-          />
-        </div>
-  
-        <div>
-          Gender: {/* NOTE: radio boxes only work when they have same `name`. */}
-          <div>
-            Male
-            <input
-            onChange = {handleChange}
-              name="gender"
-              className="male"
-              type="radio"
-              value={"male"}
-            />{" "}
-            Female{" "}
-            <input
-            onChange = {handleChange}
-              name="gender"
-              className="female"
-              type="radio"
-              value={"female"}
-            />
-          </div>
-        </div>
-        <div>
-          Age{" "}
-          <input
-          onChange = {handleChange}
-            type="number"
-            name="age"
-            className="age"
-            placeholder="enter age"
-          />
-        </div>
-        <div>
-          Tenth Score:{" "}
-          <input
-          onChange = {handleChange}
-            type="number"
-            name="tenth_score"
-            className="tenth_score"
-            placeholder="enter 10th score"
+            onChange={handleChange}
+            name="gender"
+            className="male"
+            type="radio"
+            value={"male"}
           />{" "}
-        </div>
-        <div>
-          Twelth Score:{" "}
+          Female{" "}
           <input
-          onChange = {handleChange}
-            type="number"
-            name="twelth_score"
-            className="twelth_score"
-            placeholder="enter 12th score"
-          />{" "}
+            onChange={handleChange}
+            name="gender"
+            className="female"
+            type="radio"
+            value={"female"}
+          />
         </div>
-        <div>
-          <select
-            value={""} // select dropdown needs both value and onChange attributes
-            name="preferred_branch"
-            className="preferred_branch"
-          >
-            <option value="law" onChange = {handleChange}>law</option>
-            <option value="commerce" onChange = {handleChange}>commerce</option>
-            <option value="science" onChange = {handleChange}>science</option>
-            <option value="sports" >sports</option>
-            <option value="arts">arts</option>
-            <option value="acting" >acting</option>
-          </select>
-        </div>
-  
-        <input className="submit" type="submit" value="Submit" />
-        {
-          // <div className="error"></div>
-          // show this div with proper error before submitting form, if there's anything not provided
-          // eg: first name missing, age cannot be greater than 100 etc
-        }
-      </form>
-    );
-  };
+      </div>
+      <div>
+        Age{" "}
+        <input
+          onChange={handleChange}
+          type="number"
+          name="age"
+          className="age"
+          placeholder="enter age"
+        />
+      </div>
+      <div>
+        Tenth Score:{" "}
+        <input
+          onChange={handleChange}
+          type="number"
+          name="tenth_score"
+          className="tenth_score"
+          placeholder="enter 10th score"
+        />{" "}
+      </div>
+      <div>
+        Twelth Score:{" "}
+        <input
+          onChange={handleChange}
+          type="number"
+          name="twelth_score"
+          className="twelth_score"
+          placeholder="enter 12th score"
+        />{" "}
+      </div>
+      <div>
+        <select
+          value={""} // select dropdown needs both value and onChange attributes
+          name="preferred_branch"
+          className="preferred_branch"
+        >
+          <option value="law">law</option>
+          <option value="commerce">commerce</option>
+          <option value="science">science</option>
+          <option value="sports">sports</option>
+          <option value="arts">arts</option>
+          <option value="acting">acting</option>
+        </select>
+      </div>
+
+      <input className="submit" type="submit" value="Submit" />
+      {
+        // <div className="error"></div>
+        // show this div with proper error before submitting form, if there's anything not provided
+        // eg: first name missing, age cannot be greater than 100 etc
+      }
+    </form>
+  );
+};
